18b604de9bd0828c94ddfeaab84a8468ca838e2a
TheCoder0215 <80579169+TheCoder0215@users.noreply.github.com>
2021-04-05 19:13:17 +0800

    Create template.cpp
    outlined the first template for the game

---
+++ template.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct characterStats{
+	string name;
+	bool gender;
+	string role;
+	int exp;
+	int level;
+	int hp;
+	int mp;
+	int att;
+	int def;
+	int skills[100];
+};
+
+void expUp(int exp,int level){
+	
+}
+
+
+void Menu(){
+	//TODO: if save file exists ask if player want to load the files
+	//if not, create character using characterCreation()
+	
+	
+}
+
+void initialization(){
+	
+}
+
+void characterCreation(){
+  
+	
+}
+
+void loadSave(){
+}
+
+void saveFile(char fileName[]){
+}
+
+int main () {
+	Menu();
+}
