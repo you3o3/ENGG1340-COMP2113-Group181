f2653fa8461254777f9c3ad18c846e89f42ad8ca
you3o3 <78785626+you3o3@users.noreply.github.com>
2021-04-06 20:33:55 +0800

    Move character definition
    Move character definition to creature.h and creature.cpp

--- template.cpp
+++ template.cpp
@@ -1,24 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-struct characterStats{
-	string name;
-	bool gender;
-	string role;
-	int exp;
-	int level;
-	int hp;
-	int mp;
-	int att;
-	int def;
-	int skills[100];
-};
-
-void expUp(int exp,int level){
-	
-}
-
-
 void Menu(){
 	//TODO: if save file exists ask if player want to load the files
 	//if not, create character using characterCreation()
