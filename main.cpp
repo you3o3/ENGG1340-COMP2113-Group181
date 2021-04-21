596bd76ba2186bf0bba73f85ce88055ce125c313
you3o3 <78785626+you3o3@users.noreply.github.com>
2021-04-22 02:03:03 +0800

    Add comments and small changes
    add comments to make it more readable
    correct mistakes and make some changes to make it looks consistent

--- main.cpp
+++ main.cpp
@@ -1,50 +1,55 @@
-#include<bits/stdc++.h>
-#include<cstdio>
-#include<conio.h>
-#include<math.h>
-#include<dir.h>
-#include<unistd.h>
-#include<windows.h>
-#include<dos.h>
-#include<dir.h>
-#include<ctime>
+#include <bits/stdc++.h>
+#include <cstdio>
+#include <conio.h>
+#include <math.h>
+#include <dir.h>
+#include <unistd.h>
+#include <windows.h>
+#include <dos.h>
+#include <ctime>
+#include "creature.h"
 
 using namespace std;
 
+//if file exist then return true, otherwise return false
 inline bool save_exist (const string& save) {
-    if (FILE *file = fopen(save.c_str(), "r")) {
-        fclose(file);
-        return true;
-    } else {
-        return false;
-    }   
+	if (FILE *file = fopen(save.c_str(), "r")) {
+		fclose(file);
+		return true;
+	} else {
+		return false;
+	}
 }
 
+//set color of text
 void setcolor(int ForgC){
-     WORD wColor;
-      HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
-      CONSOLE_SCREEN_BUFFER_INFO csbi;
-     if(GetConsoleScreenBufferInfo(hStdOut, &csbi)){
-          wColor = (csbi.wAttributes & 0xF0) + (ForgC & 0x0F);
-          SetConsoleTextAttribute(hStdOut, wColor);
-     }
-     return;
+	WORD wColor;
+	HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_SCREEN_BUFFER_INFO csbi;
+	if(GetConsoleScreenBufferInfo(hStdOut, &csbi)){
+		wColor = (csbi.wAttributes & 0xF0) + (ForgC & 0x0F);
+		SetConsoleTextAttribute(hStdOut, wColor);
+	}
+	return;
 }
 
+//move cursor to specific position
 void gotoxy(int column, int line){
-  COORD coord;
-  coord.X = column;
-  coord.Y = line;
-  SetConsoleCursorPosition(
-    GetStdHandle( STD_OUTPUT_HANDLE ),
-    coord
-    );
+	COORD coord;
+	coord.X = column;
+	coord.Y = line;
+	SetConsoleCursorPosition(
+		GetStdHandle( STD_OUTPUT_HANDLE ),
+		coord
+	);
 }
 
+//delay running codes
 void delay(int milliseconds) {
 	clock_t start_time = clock();
 	while(clock() < start_time + milliseconds);
 }
+
 void initialization(){
 	
 }
@@ -54,12 +59,17 @@ void characterCreation(){
 	
 }
 
+//load file from user and recover state based on the info in file
 void loadSave(){
+	
 }
 
+//save file into user's computer
 void saveFile(char fileName[]){
+	
 }
 
+//menu of the game
 void Menu(){
 	//TODO: if save file exists ask if player want to load the files
 	//if not, create character using characterCreation()
@@ -80,12 +90,13 @@ void Menu(){
 	printf("================================================================================\n");
 	setcolor(12);
 	for(i = 1; i <= 100; i++){
-        gotoxy(i/3,3);
-        if(i%3==0) printf(">>>\n");
+        	gotoxy(i/3,3);
+        	if(i%3==0)
+			printf(">>>\n");
 		gotoxy(23,4);
 		printf("%d%%", i);
 		delay(10);
-	}	
+	}
 	setcolor(10);
 	delay(500);
 	for(i = 0; i <= 4; i++){
