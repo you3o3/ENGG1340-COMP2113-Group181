d95ab8f1e3506b144a81226176dccdda10a6287f
TheCoder0215 <80579169+TheCoder0215@users.noreply.github.com>
2021-04-21 21:56:55 +0800

    Update template.cpp
    Rearrangements

--- template.cpp
+++ template.cpp
@@ -1,4 +1,5 @@
 #include<bits/stdc++.h>
+#include<cstdio>
 using namespace std;
 
 inline bool save_exist (const string& save) {
@@ -8,32 +9,6 @@ inline bool save_exist (const string& save) {
     } else {
         return false;
     }   
-}
-
-void Menu(){
-	//TODO: if save file exists ask if player want to load the files
-	//if not, create character using characterCreation()
-	
-	//Print UI Here
-	
-	if(save_exist("save.sav")){
-		char a;
-		cout << "File exists, do you want to load the save file? Y/N: ";
-		getchar(a);
-		if(a=='Y'){
-			//load file
-			loadSave();
-		} else {
-			cout << "Opening new save... Initializing character creation sequence." << endl;
-		}
-		
-	} else {
-		cout << "New game initializing... Starting character creation sequence." << endl;
-		characterCreation();
-		initialization
-	}
-	
-	
 }
 
 void initialization(){
@@ -49,6 +24,32 @@ void loadSave(){
 }
 
 void saveFile(char fileName[]){
+}
+
+void Menu(){
+	//TODO: if save file exists ask if player want to load the files
+	//if not, create character using characterCreation()
+	
+	//Print UI Here
+	
+	if(save_exist("save.sav")){
+		char a;
+		cout << "File exists, do you want to load the save file? Y/N: ";
+		a = getchar();
+		if(a=='Y'){
+			//load file
+			loadSave();
+		} else {
+			cout << "Opening new save... Initializing character creation sequence." << endl;
+		}
+		
+	} else {
+		cout << "New game initializing... Starting character creation sequence." << endl;
+		characterCreation();
+		initialization();
+	}
+	
+	
 }
 
 int main () {
