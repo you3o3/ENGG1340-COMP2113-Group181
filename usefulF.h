95b95aa85f8b7a7fea007afea0ec5e112150da0a
you3o3 <78785626+you3o3@users.noreply.github.com>
2021-04-24 12:37:58 +0800

    Add functions
    Add getxy and printBar

--- usefulF.h
+++ usefulF.h
@@ -4,6 +4,9 @@
 // clear the screen
 void clrscr();
 
+// get current cursor position
+int getxy(int *y, int *x);
+
 // move cursor to specific position
 void gotoxy(int XPos, int YPos);
 
@@ -16,12 +19,14 @@ char getch();
 // get arrow inputs (upward and downward) from user
 int getarrow();
 
-// this only works when the dimensions are known in compile time
 // print a menu like selection which will be controlled using getarrow() and enter key.
 // input: options provided, initial choice, total num of items, starting line to print
 int select(char choice[][20],int previouschosen, int totalitems, int lines);
 
 // printing strings with delay, to mimic speaking speak (user defined speed in miliseconds)
 void printDelay(std::string a, int t, bool end);
+
+// print title bar
+void printBar(std::string to_print);
 
 #endif
