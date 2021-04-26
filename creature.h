3bd855ee48af204a8fe73630be65375e5ea6794f
TheCoder0215 <80579169+TheCoder0215@users.noreply.github.com>
2021-04-26 19:20:37 +0800

    Reformatted code and changes
    added extern bool regionFlag[]

--- creature.h
+++ creature.h
@@ -2,39 +2,40 @@
 #define CREATURE_H
 
 #include "weapon.h"
+
 #include <vector>
 
-struct generalStats{
-  std::string name;
-  int level;
-  int hp, maxhp;
-  int mp, maxmp;
-  int att;
-  int def;
-  // crit_chance ranging from 0 to 100 (%)
-  int crit_chance;
-  bool isAlive();
+struct generalStats {
+    std::string name;
+    int level;
+    int hp, maxhp;
+    int mp, maxmp;
+    int att;
+    int def;
+    // crit_chance ranging from 0 to 100 (%)
+    int crit_chance;
+    bool isAlive();
 };
 
-struct character : public generalStats {
-  bool gender;
-  int xp, xpReq;
-  std::string role;
-  std::vector<Weapon> weapons;           // weapon struct in weapon.h and weapon.cpp
-  int skills[100];
-  std::string position;
-  character(std::string nameInputed = "", bool genderInputed = true);
-  void lvUp();
-  void xpUp(int);
-  void traitSet(int);
-  int traitpoints;
-  int traitAllocation[5];
+struct character: public generalStats {
+    bool gender;
+    int xp, xpReq;
+    std::string role;
+    std::vector < Weapon > weapons; // weapon struct in weapon.h and weapon.cpp
+    int skills[100];
+    std::string position;
+    character(std::string nameInputed = "", bool genderInputed = true);
+    void lvUp();
+    void xpUp(int);
+    void traitSet(int);
+    int traitpoints;
+    int traitAllocation[5];
 
 };
 
-struct monster : public generalStats {
-  std::string itemdrop;
-  int expDrop;
+struct monster: public generalStats {
+    std::string itemdrop;
+    int expDrop;
 };
 
 // usage: monster x = monsterCreation(regionGrade);
@@ -42,6 +43,6 @@ monster monsterCreation(int);
 
 extern character player;
 extern char regionMonsters[6][10][40];
-extern char regions[7][40];
-
+extern char regions[8][40];
+extern bool regionFlag[8];
 #endif
